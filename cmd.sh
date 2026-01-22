e76de6e5713d18ea4dc4096b23b2105946a88208
nandiswarb93 <nandiswarb93@gmail.com>
2026-01-22 17:53:44 +0530

    jenkins

---
+++ cmd.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+growpart /dev/nvme0n1 4
+
+lvextend -L +10G /dev/mapper/RootVG-varVol
+lvextend -L +10G /dev/mapper/RootVG-rootVol
+lvextend -l +100%FREE /dev/mapper/RootVG-homeVol
+
+xfs_growfs /
+xfs_growfs /var
+xfs_growfs /home
+
+sudo curl -o /etc/yum.repos.d/jenkins.repo \
+    https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+# Add required dependencies for the jenkins package
+sudo yum install fontconfig java-21-openjdk -y
+sudo yum install jenkins -y
+sudo systemctl daemon-reload
+sudo systemctl start jenkins
+sudo systemctl enable jenkins
